872e810ab4f56dcb5218143caa79c8b96f651d21
IceyFox006 <cgreenan@mail.bradley.edu>
2025-10-17 17:00:03 -0500

    RuffereeProgramming

--- Assets/Scripts/UI/PetRufferee.cs
+++ Assets/Scripts/UI/PetRufferee.cs
@@ -1,17 +1,45 @@
+using System.Collections;
 using UnityEngine;
 
 public class PetRufferee : MonoBehaviour
 {
-    private void FixedUpdate()
+    [SerializeField] private Animator _animator;
+
+    private Vector2 previousMousePosition = Vector2.zero;
+    private Vector2 currentMousePosition = Vector2.zero;
+    private void Start()
     {
-        RaycastHit2D hit = RaycastOn2DMousePosition();
-        if (hit)
-        {
-            Debug.Log("Hit " + hit.collider.gameObject.name);
-        }
+        StartCoroutine(CheckMovingDelay()); 
     }
     private RaycastHit2D RaycastOn2DMousePosition()
     {
         return Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+    }
+    private bool MouseIsMoving()
+    {
+        return Mathf.Abs(previousMousePosition.x - currentMousePosition.x) > 1 || Mathf.Abs(previousMousePosition.y - currentMousePosition.y) > 1; 
+    }
+    private IEnumerator CheckMovingDelay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            RaycastHit2D hit = RaycastOn2DMousePosition();
+            previousMousePosition = currentMousePosition;
+            currentMousePosition = Input.mousePosition;
+            if (hit && MouseIsMoving())
+            {
+                _animator.Play("MOVING_HOVER");
+                Debug.Log("Hit " + hit.collider.gameObject.name);
+            }
+            else if (hit && !MouseIsMoving())
+            {
+                _animator.Play("RESTING_HOVER");
+            }
+            else
+            {
+                _animator.Play("IDLE");
+            }
+        }
     }
 }
